f69bac2c6e50927f505adaead7aa0c60b6dd24d2
traceform <rafael.contato71@gmail.com>
2025-11-12 16:54:07 -0300

    new

---
+++ cisco-python-essentials-1/4.3.08.1_fuel_consumption_converter.py
@@ -0,0 +1,12 @@
+def liters_100km_to_miles_gallon(liters_100k):
+
+def miles_gallon_to_liters_100km(miles_gallon):
+
+sample_input_l100 = [3.9, 7.5, 10.]
+expected_output_l100 = [60.31143162393162,
+                        31.36194444444444,
+                        23.52145833333333]
+sample_input_mpg = [60.3, 31.4, 23.5]
+expected_output_mpg = [3.9007393587617467,
+                        7.490910297239916,
+                       10.009131205673757]
