29e3fae39011189287d496b29a9a5aff0cde5a53
traceform <rafael.contato71@gmail.com>
2025-11-12 12:04:17 -0300

    new

--- cisco-python-essentials-1/4.3.05.1_days_per_month.py
+++ cisco-python-essentials-1/4.3.05.1_days_per_month.py
@@ -1,33 +1,65 @@
 def is_year_leap(year):
+    """Returns if an year is leap or not"""
     if year < 1582:
-        msg = False  # "Not within the Gregorian calendar period"
+        leap = None  # Not within the Gregorian calendar period
     else:
         if year % 4 != 0:
-            msg = False  # "Common year"
+            leap = False  # Common year
         elif year % 100 != 0:
-            msg = True  # "Leap year"
+            leap = True  # Leap year
         elif year % 400 != 0:
-            msg = False  # "Common year"
+            leap = False  # Common year
         else:
-            msg = True  # "Leap year"
-    return msg
+            leap = True  # Leap year
+    return leap
 
 def days_in_month(year, month):
+    """Returns the amount of days in a month of a given year"""
+    # If either the year or the month is invalid
+    if year < 1582 or month < 1 or month > 12:
+        return None
+    # If it's february on a leap year
     if month == 2 and is_year_leap(year):
         return 29
     else:
+        # Only load list into memory after leap check is done
         month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
         return month_days[month - 1]
 
-test_years = [1900, 2000, 2016, 1987]
-test_months = [2, 2, 1, 11]
-test_results = [28, 29, 31, 30]
-for i in range(len(test_years)):
-    yr = test_years[i]
-    mo = test_months[i]
-    print(yr, mo, "->", end="")
-    result = days_in_month(yr, mo)
-    if result == test_results[i]:
-        print("OK")
-    else:
-        print("Failed")
+def test_is_year_leap():
+    sample_input =    [2000,  2015,  1999, 1996, 1580]
+    expected_output = [True, False, False, True, None]
+    print(">>> TESTING is_year_leap()")
+    for _ in range(len(sample_input)):
+        input = sample_input[_]
+        print(f"{input} -> ", end='')
+        output = is_year_leap(input)
+        if output == expected_output[_]:
+            status = "OK"
+        else:
+            status = "Failed"
+        print(status, end='')
+        #print(f" | Result: {output}", end='')
+        print()
+
+def test_days_in_month():
+    sample_years = [ 1900, 2000, 2015, 2016, 1999, 1996, 1580, 1987, 1945]
+    sample_months = [   2,    2,    2,    2,    1,   11,    5,    0,   13]
+    expected_output = [28,   29,   28,   29,   31,   30, None, None, None]
+    print(">>> TESTING days_in_month()")
+    for _ in range(len(sample_years)):
+        year = sample_years[_]
+        month = sample_months[_]
+        print(f"{year} {month} -> ", end='')
+        output = days_in_month(year, month)
+        if output == expected_output[_]:
+            status = "OK"
+        else:
+            status = "Failed"
+        print(status, end='')
+        #print(f" | Result: {output}", end='')
+        print()
+
+test_is_year_leap()
+print()
+test_days_in_month()
