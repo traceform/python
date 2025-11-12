da7f6786495cd6ce708ac312605670c22c742359
traceform <rafael.contato71@gmail.com>
2025-11-12 10:59:02 -0300

    new

---
+++ cisco-python-essentials-1/4.3.04.1_leap_year_function.py
@@ -0,0 +1,24 @@
+def is_year_leap(year):
+    if year < 1582:
+        msg = False  # "Not within the Gregorian calendar period"
+    else:
+        if year % 4 != 0:
+            msg = False  # "Common year"
+        elif year % 100 != 0:
+            msg = True  # "Leap year"
+        elif year % 400 != 0:
+            msg = False  # "Common year"
+        else:
+            msg = True  # "Leap year"
+    return msg
+
+test_data = [1900, 2000, 2016, 1987]
+test_results = [False, True, True, False]
+for i in range(len(test_data)):
+    yr = test_data[i]
+    print(yr,"->",end="")
+    result = is_year_leap(yr)
+    if result == test_results[i]:
+        print("OK")
+    else:
+        print("Failed")
