ab4c875760d351dc167d73a8a628b26cbf758486
traceform <rafael.contato71@gmail.com>
2025-11-13 19:25:10 -0300

    new

--- ltp/a14.2_verificador_de_senha.py
+++ ltp/a14.2_verificador_de_senha.py
@@ -15,7 +15,6 @@ def receber_dados():
 
 def verificar_senha(senha):
     """Verifica a senha"""
-
     # Verificando se a senha tem pelo menos 8 caracteres
     if len(senha) < 8:
         return False
@@ -25,7 +24,7 @@ def verificar_senha(senha):
             # Se tem pelo menos uma minúscula
             if re.search(r'[a-z]', senha):
                 # Se tem pelo menos um número
-                if re.search(r'[\d]', senha):
+                if re.search(r'[0-9]', senha):
                     # Se tem pelo menos um caractere especial
                     if re.search(r'[!@#$%^&*()]', senha):
                         return True
@@ -34,30 +33,34 @@ def verificar_senha(senha):
 def testa_verificar_senha():
     amostras_input = ['1', '12345678', '123456789', '123456789aaa', '123456789AAA', '123456789aaaAAA', '123456789aaaAAA#%$!', 'S#nh4@$T1']
     esperado = [False, False, False, False, False, False, True, True]
+
     print(f">>> TESTANDO verificar_senha()")
     for _ in range(len(esperado)):
         input = amostras_input[_]
+
         print(f"{input} -> ", end='')
+
         resultado = verificar_senha(input)
         if resultado == esperado[_]:
             estado = "OK"
         else:
             estado = "Falhou"
+
         print(estado, end='')
         print(f" | Resultado: {resultado}", end='')
         print()
 
-if __name__ == "__main__":
-    # Debugging
-    #testa_verificar_senha()
+# Debugging
+#testa_verificar_senha()
 
+if __name__ == "__main__":
     senha = receber_dados()
     forca = verificar_senha(senha)
     if forca:
         print(f"A senha é forte!")
     else:
         print(f"""A senha é fraca! Atente-se às recomendações:
-Mínimo de 8 caracteres
-Deve conter letras maiúsculas e minúsculas
-Deve conter pelo menos um número
-Deve conter pelo menos um caractere especial (!@#$%^&*())""")
+- Mínimo de 8 caracteres
+- Deve conter letras maiúsculas e minúsculas
+- Deve conter pelo menos um número
+- Deve conter pelo menos um caractere especial (!@#$%^&*())""")
