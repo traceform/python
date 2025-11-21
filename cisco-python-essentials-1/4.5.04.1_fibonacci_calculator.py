1dd9c4dc0f896e98e6026d69816f2824d56e6b36
traceform <rafael.contato71@gmail.com>
2025-11-21 10:49:40 -0300

    new

---
+++ cisco-python-essentials-1/4.5.04.1_fibonacci_calculator.py
@@ -0,0 +1,88 @@
+# --- Utility functions ---
+def fib(num):
+    """Returns the fibonacci of a given number"""
+    # Must be an integer
+    if not isinstance(num, int):
+        return None
+    else:
+        # Rule: must be positive in this case
+        if num < 0:
+            return None
+        else:
+            # Rule: Fibonacci of 0 is 0 itself
+            if num == 0:
+                return 0
+            # Rule: Fibonacci of both 1 and 2 is 1
+            elif 1 <= num <= 2:
+                return 1
+            else:
+                # Rule: each Fibonacci number is the sum
+                # of the two previous Fibonacci numbers
+                fibonacci = 1
+                for n in range(num, num + 1):
+                    return fib(n - 1) + fib(n - 2)
+
+# --- Input/output functions ---
+def receive_num() -> int:
+    """Receives, validates and returns the user input"""
+    while True:
+        try:
+            n = int(input("Enter a non-negative integer: "))
+            if not n >= 0:
+                raise ValueError
+            return n
+        except ValueError:
+            print("Only positive integers are accepted. ", end='')
+        except KeyboardInterrupt:
+            print("\nProgram terminated.")
+            quit()
+
+# --- Test functions ---
+def test_function(function, test_type, sample_input, expected_output):
+    """Tests a given function"""
+    print(f">>> TESTING FOR [{test_type.upper()}] {function}")
+    for _ in range(len(expected_output)):
+        input = sample_input[_]
+        expected = expected_output[_]
+
+        print(f"Number: {input} -> ", end='')
+        output = function(input)
+        if output == expected:
+            status = 'OK'
+        else:
+            status = 'Failed'
+        
+        print(status, end='')
+        print(f" | Result: {output}", end='')
+        print()
+
+def test_fib():
+    print(">>> Trying the first 10 fibonacci numbers: ")
+    for n in range(0, 10):
+        print(fib(n), end=' ')
+    print()
+
+    function = fib
+
+    # Regular cases
+    test_type = "Regular cases"
+    sample_input = [   0, 1, 2, 3, 4, 5, 6,  7]
+    expected_output = [0, 1, 1, 2, 3, 5, 8, 13]
+    test_function(function, test_type, sample_input, expected_output)
+
+    # Edge cases
+    test_type = "Edge cases"
+    sample_input = [     -1,  1.0,  'a']
+    expected_output = [None, None, None]
+    test_function(function, test_type, sample_input, expected_output)
+
+# --- Master ---
+def master():
+    n = receive_num()
+    print(fib(n))
+
+if __name__ == '__main__':
+    # Debugging
+    #test_fib()
+
+    master()
